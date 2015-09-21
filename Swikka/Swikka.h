b9554e5e08467338761aacd1b5f993083d4f8ac0
Ryuichi Saito <ryuichi@ryuichisaito.com>
2015-09-21 16:05:11 -0700

    Initial commit

---
+++ Swikka/Swikka.h
@@ -0,0 +1,19 @@
+//
+//  Swikka.h
+//  Swikka
+//
+//  Created by Ryuichi Saito on 9/21/15.
+//  Copyright © 2015 Ryuichi Saito, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swikka.
+FOUNDATION_EXPORT double SwikkaVersionNumber;
+
+//! Project version string for Swikka.
+FOUNDATION_EXPORT const unsigned char SwikkaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swikka/PublicHeader.h>
+
+
